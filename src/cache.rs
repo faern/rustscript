038172aba988828d35d250bf2ed802d95ff43ac4
Linus Färnstrand <faern@faern.net>
2016-12-29 16:31:42 +0100

    Use error-chain for error handling

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,8 @@
 use app_dirs::{app_root, AppInfo, AppDataType};
 use std::path::PathBuf;
 
+use {ResultExt, Result};
+
 const APP_INFO: AppInfo = AppInfo {
     name: "rustscript",
     author: "33c3",
@@ -11,8 +13,9 @@ pub struct BinCache {
 }
 
 impl BinCache {
-    pub fn new() -> Self {
-        BinCache { cache_dir: app_root(AppDataType::UserCache, &APP_INFO).expect("No cache dir") }
+    pub fn new() -> Result<Self> {
+        let cache_dir = app_root(AppDataType::UserCache, &APP_INFO).chain_err(|| "No cache dir")?;
+        Ok(BinCache { cache_dir: cache_dir })
     }
 
     pub fn get(&self, hash: String) -> PathBuf {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+#![recursion_limit = "1024"]
+
 extern crate app_dirs;
 extern crate crypto;
+#[macro_use]
+extern crate error_chain;
+
+mod errors {
+    error_chain!{}
+}
+
+use errors::*;
 
 mod cache;
 mod hash;
@@ -9,38 +19,51 @@ use std::process::{self, Command};
 use std::path::Path;
 
 fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     let mut args = args();
-    let _bin_path = args.next().expect("wtf?");
-    let script_path = args.next().expect("No script given");
+    let _bin_path = args.next().ok_or("rustscript not given as first argument")?;
+    let script_path = args.next().ok_or("No script given to run")?;
     let script_args: Vec<String> = args.collect();
 
-    let hash = hash::hash(&script_path).unwrap();
+    let hash = hash::hash(&script_path).chain_err(|| "Unable to take signature of script")?;
 
-    let cache = cache::BinCache::new();
+    let cache = cache::BinCache::new()?;
     let script_bin_path = cache.get(hash);
     if !script_bin_path.exists() {
-        if let Err(e) = compile(&script_path, &script_bin_path) {
-            println!("Unable to compile the script: {}", e);
-            process::exit(1);
-        }
+        println!("Script not found in cache, compiling");
+        compile(&script_path, &script_bin_path).chain_err(|| "Unable to compile the script")?;
     }
 
     println!("Running {} with {:?}", &script_path, script_args);
 
-    let mut script_process =
-        Command::new("./a.out").args(&script_args).spawn().expect("Unable to spawn script");
-    script_process.wait().expect("Script crashed");
+    let mut script_process = Command::new(&script_bin_path).args(&script_args)
+        .spawn()
+        .chain_err(|| "Unable to spawn script")?;
+    let script_result = script_process.wait().chain_err(|| "Script crashed")?;
+    process::exit(script_result.code().unwrap());
 }
 
-fn compile<P: AsRef<Path>, P2: AsRef<Path>>(script_path: P, output_path: P2) -> Result<(), String> {
+fn compile<P: AsRef<Path>, P2: AsRef<Path>>(script_path: P, output_path: P2) -> Result<()> {
     let mut rustc = Command::new("rustc").arg(script_path.as_ref())
         .arg("-o")
         .arg(output_path.as_ref())
         .spawn()
-        .map_err(|_| "Unable to spawn rustc")?;
-    let rustc_result = rustc.wait().map_err(|_| "Rustc crashed")?;
+        .chain_err(|| "Unable to spawn rustc")?;
+    let rustc_result = rustc.wait().chain_err(|| "rustc crashed")?;
     if !rustc_result.success() {
-        Err("Rustc did not build successfully".to_owned())
+        bail!("rustc did not build successfully")
     } else {
         Ok(())
     }
